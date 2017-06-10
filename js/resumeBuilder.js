ce7f3be60fcc0e0c96007fa9e2c5f5398571faed
Mahesh Hariharasubramanian <mascor1331@gmail.com>
2017-06-10 15:36:25 -0400

    added work, projects, bio and education objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,40 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var work = {
+	"jobs": [{
+		"employer": "NYPIUA",
+		"title": "IT Intern",
+		"location": "100 William Street, New York",
+		"dates": "15 May to 1 September 2015",
+		"description": "Web services"
+	}]
+};
+var projects = {
+	"projects": [{
+		"title": "Web services testing",
+		"dates": "15 May to 6 June 2017",
+		"description": "rest and soap web services testing usin SOAPUI"
+	}]
+};
+var bio = {
+	"name": "Mahesh Hariharasubramanian",
+	"role": "Web Developer",
+	"welcomeMessage": "Welcome to my resume",
+	"biopic": "images/fry.jpg"
+};
+var education = {
+	"schools": [{
+		"name": "Rutgers University",
+		"location": "New Brunswick",
+		"degree dates": "September 2016 - May 2018",
+		"url": "http://www.rutgers.edu",
+		"majors": ["Electrical and Computer Engineering"]
+	}],
+	"onlineCourses": [{
+		"title": "Intro to JS",
+		"school": "Udacity",
+		"dates": "Summer 2017",
+		"url": "http://www.udacity.com"
+	}]
+};
